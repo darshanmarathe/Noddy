823cf0b130d316b6431b70832d32de19eaf562e0
Darshan Marathe <darshanmarathe@zoho.com>
2013-10-04 17:00:16 +0530

    Added Support for handling 404
    Added Support for handling 401

--- app.js
+++ app.js
@@ -53,6 +53,25 @@ DefineRoughts(app , routes_index);
 DefineRoughts(app , routes_user);
 DefineRoughts(app , routes_members);
 
+app.use(function(req, res, next){
+  res.status(404);
+
+  // respond with html page
+  if (req.accepts('html')) {
+    res.render('404', { url: req.url });
+    return;
+  }
+
+  // respond with json
+  if (req.accepts('json')) {
+    res.send({ error: 'Not found' });
+    return;
+  }
+
+  // default to plain-text. send()
+  res.type('txt').send('Not found');
+})
+
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
