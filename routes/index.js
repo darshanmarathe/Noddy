3f3575ed5d43577fef5e459494ef9f652ba236fb
Darshan Marathe <darshanamrathe@zoho.com>
2013-10-22 01:16:32 -0400

    Iframe breck code added for noddy

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,8 @@
  * GET home page.
  */
 
+var _userRepo = require("./../Repos/userRepo");
+
 exports.index = function(req, res) {
     if (req.isAuthenticated()) {
         res.render('index', {
@@ -37,10 +39,33 @@ exports.api = function(req, res) {
 
 }
 
+
+exports.Profile = function(req, res) {
+    _userRepo.getuser(req.params.id, function(user) {
+        console.log(user);
+        if (req.isAuthenticated()) {
+            res.render('profile', {
+                title: 'Welcome to Noddy - your node js stop',
+                user: req.user[0],
+                model: user[0],
+                layout: 'userLayout'
+            });
+        }
+        else {
+            res.render('profile', {
+                title: 'Welcome to Noddy - your node js stop',
+                model: user[0]
+            });
+        }
+    });
+
+}
+
 exports.define = function(app, routes) {
     app.get('/', routes.index);
     app.get('/api', routes.api)
     app.get('/index', routes.index);
     app.get('/about', routes.about);
     app.get('/contact', routes.contact);
+    app.get('/Profile/:id', routes.Profile)
 }
